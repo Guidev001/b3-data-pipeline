0160761c30a4f508c2c60179232eb6dcdba07e23
Guilherme Gomes David <guidev001@gmail.com>
2025-02-19 18:41:16 -0300

    feature: implement job every day at 10h and 18h

--- src/main.py
+++ src/main.py
@@ -2,20 +2,26 @@ from scraping.scraper import get_b3_data
 from storage.s3_manager import upload_to_s3
 from etl.transform import clean_data, save_parquet
 from datetime import datetime
+import schedule
+import time
 
-# Passo 1: Coletar os dados da B3
-df = get_b3_data()
 
-# Passo 2: Transformar os dados
-df = clean_data(df)
+def job():
+    df = get_b3_data()
 
-# Nome do arquivo baseado na data atual
-date_str = datetime.today().strftime('%Y-%m-%d')
-filename = f"b3_data_{date_str}.parquet"
+    df = clean_data(df)
 
-# Passo 3: Salvar como Parquet
-save_parquet(df, filename)
+    date_str = datetime.today().strftime('%Y-%m-%d')
+    filename = f"b3_data_{date_str}.parquet"
 
-# Passo 4: Enviar para o AWS S3
-s3_path = f"raw/{date_str}/{filename}"
-upload_to_s3(filename, s3_path)
+    save_parquet(df, filename)
+
+    s3_path = f"raw/{date_str}/{filename}"
+    upload_to_s3(filename, s3_path)
+
+schedule.every().day.at("10:00").do(job)
+schedule.every().day.at("18:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(120)
